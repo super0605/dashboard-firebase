40a6c58f7eb6df0459c74ece1ff245052039eee6
super0605 <36216137+super0605@users.noreply.github.com>
2020-10-16 22:47:32 +0800

    Update firebase.js
    Remove credentials

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,14 +2,14 @@ import * as firebase from "firebase";
 import "firebase/database";
 
 let config = {
-  apiKey: "AIzaSyCfMxUjXgM5t2ogzJV2zLfBJwTeVr00iCs",
-  authDomain: "nextgatetechtest-26090.firebaseapp.com",
-  databaseURL: "https://nextgatetechtest-26090.firebaseio.com",
-  projectId: "nextgatetechtest-26090",
-  storageBucket: "nextgatetechtest-26090.appspot.com",
-  messagingSenderId: "725828408121",
-  appId: "1:725828408121:web:a6a04df979d1d9abc30ee8",
-  measurementId: "G-S6NXY3ECQT",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 firebase.initializeApp(config);
